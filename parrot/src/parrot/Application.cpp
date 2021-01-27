3e0403de4e5ee7e7e3e1012f6aa664e242887cf5
Milo <milo.h.xu@icloud.com>
2021-01-27 22:41:51 +0800

    Vertex Buffer Layout abstraction

--- parrot/src/parrot/Application.cpp
+++ parrot/src/parrot/Application.cpp
@@ -9,6 +9,24 @@ namespace parrot {
     // Init Singleton
     Application* Application::s_instance = nullptr;
 
+    // Temporary
+    static GLenum shaderDataTypeToOpenGLBaseType(ShaderDataType type) {
+        switch (type) {
+            case parrot::ShaderDataType::None  : return GL_FLOAT;
+            case parrot::ShaderDataType::Float : return GL_FLOAT;
+            case parrot::ShaderDataType::Float2: return GL_FLOAT;
+            case parrot::ShaderDataType::Float3: return GL_FLOAT;
+            case parrot::ShaderDataType::Float4: return GL_FLOAT;
+            case parrot::ShaderDataType::Mat3  : return GL_FLOAT;
+            case parrot::ShaderDataType::Mat4  : return GL_FLOAT;
+            case parrot::ShaderDataType::Int   : return GL_INT;
+            case parrot::ShaderDataType::Int2  : return GL_INT;
+            case parrot::ShaderDataType::Int3  : return GL_INT;
+            case parrot::ShaderDataType::Int4  : return GL_INT;
+            case parrot::ShaderDataType::Bool  : return GL_BOOL;
+        }
+    }
+
     Application::Application() {
         PR_INT_ASSERT(!s_instance, "Application already exist!")
         s_instance = this;
@@ -22,15 +40,34 @@ namespace parrot {
         glBindVertexArray(m_vertex_array);
 
         // Vertex Buffer
-        float vertices[3 * 3] = {
-            -0.5f, -0.5f, 0.0f,
-             0.5f, -0.5f, 0.0f,
-             0.0f,  0.5f, 0.0f
+        float vertices[3 * 7] = {
+            -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+             0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+             0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f
         };
         m_vertex_buffer.reset(VertexBuffer::create(vertices, sizeof(vertices)));
 
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+        BufferLayout layout = {
+            { ShaderDataType::Float3, "a_position" },
+            { ShaderDataType::Float4, "a_color"    }
+        };
+
+        m_vertex_buffer->setLayout(layout);
+
+        const auto& vertex_layout = m_vertex_buffer->getLayout();
+        uint32_t index = 0;
+        for (const auto& element : vertex_layout) {
+            glEnableVertexAttribArray(index);
+            glVertexAttribPointer(
+                index, 
+                element.getComponentCount(), 
+                shaderDataTypeToOpenGLBaseType(element.type), 
+                element.normalized ? GL_TRUE : GL_FALSE, 
+                vertex_layout.getStride(), 
+                (const void*)(element.offset)
+            );
+            ++index;
+        }
 
         // Index Buffer
         uint32_t indices[3] = {
@@ -41,9 +78,14 @@ namespace parrot {
         std::string vertex_source = R"(
             #version 330 core
             layout(location = 0) in vec3 a_position;
+            layout(location = 1) in vec4 a_color;
+
             out vec3 v_position;
+            out vec4 v_color;
+
             void main() {
                 v_position  = a_position;
+                v_color     = a_color;
                 gl_Position = vec4(a_position, 1.0);
             }
         )";
@@ -52,8 +94,10 @@ namespace parrot {
             #version 330 core
             layout(location = 0) out vec4 color;
             in vec3 v_position;
+            in vec4 v_color;
             void main() {
                 color = vec4(v_position * 0.5 + 0.5, 1.0);
+                color = v_color;
             }
         )";           
 
--- parrot/src/platform/OpenGL/OpenGLBuffer.h
+++ parrot/src/platform/OpenGL/OpenGLBuffer.h
@@ -9,10 +9,19 @@ namespace parrot {
         OpenGLVertexBuffer(float* vertices, uint32_t size);
         virtual ~OpenGLVertexBuffer();
 
-        virtual void bind()   const;
-        virtual void unbind() const;
+        virtual void bind()   const override;
+        virtual void unbind() const override;
+
+        virtual void                setLayout(const BufferLayout& layout) override {
+            m_layout = layout;
+        }
+        virtual const BufferLayout& getLayout() const override {
+            return m_layout;
+        }
+
     private:
-        uint32_t m_renderer_id = 0;
+        uint32_t     m_renderer_id = 0;
+        BufferLayout m_layout;
     };
 
     class OpenGLIndexBuffer : public IndexBuffer {
@@ -20,9 +29,9 @@ namespace parrot {
         OpenGLIndexBuffer(uint32_t* indices, uint32_t count);
         virtual ~OpenGLIndexBuffer();
 
-        virtual void     bind    () const;
-        virtual void     unbind  () const;
-        virtual uint32_t getCount() const { return m_count; }
+        virtual void     bind    () const override;
+        virtual void     unbind  () const override;
+        virtual uint32_t getCount() const override { return m_count; }
     private:
         uint32_t m_renderer_id = 0;
         uint32_t m_count = 0;
