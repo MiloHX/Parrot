68937725b4a082b2086ce08cff9e02f5f715cd75
Milo <milo.h.xu@icloud.com>
2021-01-31 08:55:33 +0800

    Batch Rendering Phase 1

--- parrot/src/parrot/renderer/Renderer2D.cpp
+++ parrot/src/parrot/renderer/Renderer2D.cpp
@@ -6,56 +6,69 @@
 
 namespace parrot {
 
-    struct Renderer2Data {
-        Ref<VertexArray> vertex_array;
-        Ref<Shader>      texture_shader;
-        Ref<Texture2D>   white_texture;
+    struct QuadVertex {
+        glm::vec3 position;
+        glm::vec4 color;
+        glm::vec2 text_coord;
     };
 
-    static Renderer2Data* s_data;
+    struct Renderer2Data {
+        const uint32_t max_quads_per_draw = 16384;
+        const uint32_t max_vertices_per_draw = max_quads_per_draw * 4;
+        const uint32_t max_indices_per_draw  = max_quads_per_draw * 6;
+
+        Ref<VertexArray>  vertex_array;
+        Ref<VertexBuffer> vertex_buffer;
+        Ref<Shader>       texture_shader;
+        Ref<Texture2D>    white_texture;
+
+        uint32_t    index_count           = 0;
+        QuadVertex* vertex_buffer_begin   = nullptr;
+        QuadVertex* vertex_buffer_pointer = nullptr;
+    };
+
+    static Renderer2Data s_data;
 
     void Renderer2D::init() {
-        s_data = new Renderer2Data();
-        // Triangle Vertex Array
-        s_data->vertex_array = VertexArray::create();
+        // Vertex Array
+        s_data.vertex_array = VertexArray::create();
 
-        float vertices[5 * 4] = {
-            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
-             0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-             0.5f,  0.5f, 0.0f, 1.0f, 1.0f,
-            -0.5f,  0.5f, 0.0f, 0.0f, 1.0f
+        float vertices[9 * 4] = {
+            -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+             0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+             0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+            -0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f
         };
 
-        Ref<parrot::VertexBuffer> vertex_buffer;
-        vertex_buffer = VertexBuffer::create(vertices, sizeof(vertices));
-        vertex_buffer->setLayout({
+        s_data.vertex_buffer = VertexBuffer::create(vertices, sizeof(vertices));
+        s_data.vertex_buffer->setLayout({
             { ShaderDataType::Float3, "a_position"  },
+            { ShaderDataType::Float4, "a_color"     },
             { ShaderDataType::Float2, "a_tex_coord" }
         });
-        s_data->vertex_array->addVertexBuffer(vertex_buffer);
+        s_data.vertex_array->addVertexBuffer(s_data.vertex_buffer);
 
         uint32_t indices[6] = { 0, 1, 2, 2, 3, 0 };
 
         Ref<IndexBuffer> index_buffer;
         index_buffer = IndexBuffer::create(indices, sizeof(indices) / sizeof(uint32_t));
-        s_data->vertex_array->setIndexBuffer(index_buffer);
+        s_data.vertex_array->setIndexBuffer(index_buffer);
 
-        s_data->white_texture = Texture2D::create(1, 1);
+        s_data.white_texture = Texture2D::create(1, 1);
         uint32_t white_texture_data = 0xffffffff;
-        s_data->white_texture->setData(&white_texture_data, sizeof(uint32_t));
+        s_data.white_texture->setData(&white_texture_data, sizeof(uint32_t));
 
-        s_data->texture_shader = Shader::create("asset/shader/texture_shader.glsl"   );
-        s_data->texture_shader->bind();
-        s_data->texture_shader->setInt("u_texture", 0);
+        s_data.texture_shader = Shader::create("asset/shader/texture_shader.glsl"   );
+        s_data.texture_shader->bind();
+        s_data.texture_shader->setInt("u_texture", 0);
     }
 
     void Renderer2D::shutdown() {
-        delete s_data;
     }
 
     void Renderer2D::beginScene(const OrthographicCamera camera) {
-        s_data->texture_shader->bind();
-        s_data->texture_shader->setMat4("u_view_projection", camera.getViewPorjectionMatrix());
+        s_data.texture_shader->bind();
+        s_data.texture_shader->setMat4("u_view_projection", camera.getViewPorjectionMatrix());
     }
 
     void Renderer2D::endScene() {
@@ -69,11 +82,11 @@ namespace parrot {
         const glm::vec4&      color, 
         const glm::vec2&      texture_scale
     ) {
-        s_data->texture_shader->setFloat4("u_color", color);
+        s_data.texture_shader->setFloat4("u_color", color);
         if (texture) {
             texture->bind();
         } else {
-            s_data->white_texture->bind();
+            s_data.white_texture->bind();
         }
         glm::mat4 transform;
         if (rotation == 0) {
@@ -86,10 +99,10 @@ namespace parrot {
                 glm::rotate(glm::mat4(1.0f), rotation, glm::vec3{ 0.0f, 0.0f, 1.0f }) *
                 glm::scale(glm::mat4(1.0f), glm::vec3{ size.x, size.y, 1.0f });
         }
-        s_data->texture_shader->setMat4("u_transform", transform);
-        s_data->texture_shader->setFloat2("u_texture_scale", texture_scale);
+        s_data.texture_shader->setMat4("u_transform", transform);
+        s_data.texture_shader->setFloat2("u_texture_scale", texture_scale);
 
-        s_data->vertex_array->bind();
-        RenderCommand::drawIndexed(s_data->vertex_array);
+        s_data.vertex_array->bind();
+        RenderCommand::drawIndexed(s_data.vertex_array);
     }
 }
--- sandbox/asset/shader/texture_shader.glsl
+++ sandbox/asset/shader/texture_shader.glsl
@@ -1,11 +1,13 @@
 #type vertex
 #version 330 core
 layout(location = 0) in vec3 a_position;
-layout(location = 1) in vec2 a_tex_coord;
+layout(location = 1) in vec3 a_color;
+layout(location = 2) in vec2 a_tex_coord;
     
 uniform mat4 u_view_projection;
 uniform mat4 u_transform;
 
+out vec4 v_color;
 out vec2 v_tex_coord;
 
 void main() {
@@ -18,11 +20,13 @@ void main() {
 #version 330 core
 layout(location = 0) out vec4 color;
 
+in vec4 v_color;
+in vec2 v_tex_coord;
+
 uniform vec4      u_color;
 uniform vec2      u_texture_scale;
 uniform sampler2D u_texture;
 
-in vec2 v_tex_coord;
 
 void main() {
     color = texture(u_texture, v_tex_coord * u_texture_scale) * u_color;
