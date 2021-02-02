4ff9b25ef9380a7d853b2652fb0581b6f06a58a9
Milo <milo.h.xu@icloud.com>
2021-02-02 21:26:56 +0800

    Naming Refactor

--- parrot/src/parrot/renderer/Renderer2D.cpp
+++ parrot/src/parrot/renderer/Renderer2D.cpp
@@ -14,51 +14,38 @@ namespace parrot {
 
     struct Renderer2Data {
         const uint32_t max_quads_per_draw = 16384;
-        const uint32_t max_vertices_per_draw = max_quads_per_draw * 4;
-        const uint32_t max_indices_per_draw  = max_quads_per_draw * 6;
+        const uint32_t max_quad_vertices_per_draw = max_quads_per_draw * 4;
+        const uint32_t max_quad_indices_per_draw  = max_quads_per_draw * 6;
 
         Ref<VertexArray>  vertex_array;
-        Ref<VertexBuffer> vertex_buffer;
-        Ref<Shader>       texture_shader;
+        Ref<VertexBuffer> quad_vertex_buffer;
+        Ref<Shader>       quad_texture_shader;
         Ref<Texture2D>    white_texture;
 
-        uint32_t    index_count           = 0;
-        QuadVertex* vertex_buffer_begin   = nullptr;
-        QuadVertex* vertex_buffer_pointer = nullptr;
+        uint32_t    quad_index_count      = 0;
+        QuadVertex* quad_vertices         = nullptr;
+        QuadVertex* quad_vertices_pointer = nullptr;
     };
 
     static Renderer2Data s_data;
-
-    //float vertices[9 * 4] = {
-    //    -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-    //     0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-    //     0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
-    //    -0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f
-    //};
 
     void Renderer2D::init() {
         // Vertex Array
         s_data.vertex_array = VertexArray::create();
 
-
-
-        //s_data.vertex_buffer = VertexBuffer::create(vertices, sizeof(vertices));
-        s_data.vertex_buffer = VertexBuffer::create(s_data.max_vertices_per_draw * sizeof(QuadVertex));
-        //s_data.vertex_buffer = VertexBuffer::create(sizeof(vertices));
-        s_data.vertex_buffer->setLayout({
+        s_data.quad_vertex_buffer = VertexBuffer::create(s_data.max_quad_vertices_per_draw * sizeof(QuadVertex));
+        s_data.quad_vertex_buffer->setLayout({
             { ShaderDataType::Float3, "a_position"  },
             { ShaderDataType::Float4, "a_color"     },
             { ShaderDataType::Float2, "a_tex_coord" }
         });
-        s_data.vertex_array->addVertexBuffer(s_data.vertex_buffer);
+        s_data.vertex_array->addVertexBuffer(s_data.quad_vertex_buffer);
 
-        s_data.vertex_buffer_begin = new QuadVertex[s_data.max_vertices_per_draw];
-        // s_data.vertex_buffer->setData(vertices, sizeof(vertices));
+        s_data.quad_vertices = new QuadVertex[s_data.max_quad_vertices_per_draw];
 
-        //uint32_t indices[6] = { 0, 1, 2, 2, 3, 0 };
-        uint32_t* quad_indices = new uint32_t[s_data.max_indices_per_draw];
+        uint32_t* quad_indices = new uint32_t[s_data.max_quad_indices_per_draw];
         uint32_t offset = 0;
-        for (uint32_t i = 0; i < s_data.max_indices_per_draw; i += 6) {
+        for (uint32_t i = 0; i < s_data.max_quad_indices_per_draw; i += 6) {
             quad_indices[i + 0] = offset + 0;
             quad_indices[i + 1] = offset + 1;
             quad_indices[i + 2] = offset + 2;
@@ -70,39 +57,38 @@ namespace parrot {
         }
 
         Ref<IndexBuffer> index_buffer;
-        index_buffer = IndexBuffer::create(quad_indices, s_data.max_indices_per_draw);
+        index_buffer = IndexBuffer::create(quad_indices, s_data.max_quad_indices_per_draw);
         s_data.vertex_array->setIndexBuffer(index_buffer);
+        delete[] quad_indices;
 
         s_data.white_texture = Texture2D::create(1, 1);
         uint32_t white_texture_data = 0xffffffff;
         s_data.white_texture->setData(&white_texture_data, sizeof(uint32_t));
 
-        s_data.texture_shader = Shader::create("asset/shader/texture_shader.glsl"   );
-        s_data.texture_shader->bind();
-        s_data.texture_shader->setInt("u_texture", 0);
+        s_data.quad_texture_shader = Shader::create("asset/shader/texture_shader.glsl"   );
+        s_data.quad_texture_shader->bind();
+        s_data.quad_texture_shader->setInt("u_texture", 0);
     }
 
     void Renderer2D::shutdown() {
     }
 
     void Renderer2D::beginScene(const OrthographicCamera camera) {
-        s_data.texture_shader->bind();
-        s_data.texture_shader->setMat4("u_view_projection", camera.getViewPorjectionMatrix());
+        s_data.quad_texture_shader->bind();
+        s_data.quad_texture_shader->setMat4("u_view_projection", camera.getViewPorjectionMatrix());
 
-        s_data.index_count = 0;
-        s_data.vertex_buffer_pointer = s_data.vertex_buffer_begin;
+        s_data.quad_index_count = 0;
+        s_data.quad_vertices_pointer = s_data.quad_vertices;
     }
 
     void Renderer2D::endScene() {
-        uint32_t data_size = (uint8_t*)s_data.vertex_buffer_pointer - (uint8_t*)s_data.vertex_buffer_begin;
-        //uint32_t data_size = 36;
-        s_data.vertex_buffer->setData(s_data.vertex_buffer_begin, data_size);
-        //s_data.vertex_buffer->setData(vertices, sizeof(vertices));
+        uint32_t data_size = (uint8_t*)s_data.quad_vertices_pointer - (uint8_t*)s_data.quad_vertices;
+        s_data.quad_vertex_buffer->setData(s_data.quad_vertices, data_size);
         flush();
     }
 
     void Renderer2D::flush(){
-        RenderCommand::drawIndexed(s_data.vertex_array, s_data.index_count); // Temp
+        RenderCommand::drawIndexed(s_data.vertex_array, s_data.quad_index_count);
     }
 
     void Renderer2D::drawQuad(
@@ -113,29 +99,29 @@ namespace parrot {
         const glm::vec4&      color, 
         const glm::vec2&      texture_scale
     ) {
-        s_data.vertex_buffer_pointer->position   = position;// glm::vec3{-0.5f, -0.5f, 0.0f };
-        s_data.vertex_buffer_pointer->color      = color; // glm::vec4{ 1.0f,  1.0f, 1.0f, 1.0f };
-        s_data.vertex_buffer_pointer->text_coord = glm::vec2{ 0.0f, 0.0f };
-        s_data.vertex_buffer_pointer++;
+        s_data.quad_vertices_pointer->position   = position;
+        s_data.quad_vertices_pointer->color      = color;
+        s_data.quad_vertices_pointer->text_coord = glm::vec2{ 0.0f, 0.0f };
+        s_data.quad_vertices_pointer++;
 
-        s_data.vertex_buffer_pointer->position   = glm::vec3{ position.x + size.x, position.y, position.z }; //glm::vec3{ 0.5f, -0.5f, 0.0f };
-        s_data.vertex_buffer_pointer->color      = color; // glm::vec4{ 1.0f,  1.0f, 1.0f, 1.0f };
-        s_data.vertex_buffer_pointer->text_coord = glm::vec2{ 1.0f, 0.0f };
-        s_data.vertex_buffer_pointer++;
+        s_data.quad_vertices_pointer->position   = glm::vec3{ position.x + size.x, position.y, position.z };
+        s_data.quad_vertices_pointer->color      = color;
+        s_data.quad_vertices_pointer->text_coord = glm::vec2{ 1.0f, 0.0f };
+        s_data.quad_vertices_pointer++;
 
-        s_data.vertex_buffer_pointer->position   = glm::vec3{ position.x + size.x, position.y + size.y, position.z }; // glm::vec3{ 0.5f,  0.5f, 0.0f };
-        s_data.vertex_buffer_pointer->color      = color; // glm::vec4{ 1.0f,  1.0f, 1.0f, 1.0f };
-        s_data.vertex_buffer_pointer->text_coord = glm::vec2{ 1.0f, 1.0f };
-        s_data.vertex_buffer_pointer++;
+        s_data.quad_vertices_pointer->position   = glm::vec3{ position.x + size.x, position.y + size.y, position.z };
+        s_data.quad_vertices_pointer->color      = color;
+        s_data.quad_vertices_pointer->text_coord = glm::vec2{ 1.0f, 1.0f };
+        s_data.quad_vertices_pointer++;
 
-        s_data.vertex_buffer_pointer->position   = glm::vec3{ position.x, position.y + size.y, position.z }; // glm::vec3{-0.5f,  0.5f, 0.0f };
-        s_data.vertex_buffer_pointer->color      = color; // glm::vec4{ 1.0f,  1.0f, 1.0f, 1.0f };
-        s_data.vertex_buffer_pointer->text_coord = glm::vec2{ 0.0f, 1.0f };
-        s_data.vertex_buffer_pointer++;
+        s_data.quad_vertices_pointer->position   = glm::vec3{ position.x, position.y + size.y, position.z };
+        s_data.quad_vertices_pointer->color      = color;
+        s_data.quad_vertices_pointer->text_coord = glm::vec2{ 0.0f, 1.0f };
+        s_data.quad_vertices_pointer++;
 
-        s_data.index_count +=6;
+        s_data.quad_index_count +=6;
 
-        //s_data.texture_shader->setFloat4("u_color", color);
+        //s_data.quad_texture_shader->setFloat4("u_color", color);
         //if (texture) {
         //    texture->bind();
         //} else {
@@ -152,8 +138,8 @@ namespace parrot {
         //        glm::rotate(glm::mat4(1.0f), rotation, glm::vec3{ 0.0f, 0.0f, 1.0f }) *
         //        glm::scale(glm::mat4(1.0f), glm::vec3{ size.x, size.y, 1.0f });
         //}
-        //s_data.texture_shader->setMat4("u_transform", transform);
-        //s_data.texture_shader->setFloat2("u_texture_scale", texture_scale);
+        //s_data.quad_texture_shader->setMat4("u_transform", transform);
+        //s_data.quad_texture_shader->setFloat2("u_texture_scale", texture_scale);
 
         //s_data.vertex_array->bind();
         //RenderCommand::drawIndexed(s_data.vertex_array);
